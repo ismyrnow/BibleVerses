ad87ea0889e63bf3c707c31ae4c8de54c7f1b5c7
Ishmael Smyrnow <ismyrnow@gmail.com>
2014-03-17 00:58:52 -0400

    View transitions, mobile tweaks

--- public/js/main.js
+++ public/js/main.js
@@ -34,6 +34,33 @@ require.config({
 require(['backbone', 'marionette', 'router', 'collections/verses-collection'],
 function (Backbone, Marionette, Router, VersesCollection) {
   window.App = new Marionette.Application();
+  
+  Marionette.Region.prototype.open = function (view) {
+    var $viewEl = $(view.el);
+    this.$el.append($viewEl);
+    setTimeout(function () {
+      $viewEl.addClass('transition-in');
+    }, 1);
+  };
+  
+  Marionette.Region.prototype.close = function(){
+    var view = this.currentView;
+    if (!view || view.isClosed){ return; }
+    
+    var $viewEl = $(this.currentView.el);
+    $viewEl.removeClass('transition-in').addClass('transition-out');
+    setTimeout(function () {      
+
+      // call 'close' or 'remove', depending on which is found
+      if (view.close) { view.close(); }
+      else if (view.remove) { view.remove(); }
+
+      Marionette.triggerMethod.call(this, 'close');
+
+      delete this.currentView;
+      
+    }, 700);
+  }
 
   App.addRegions({
     mainRegion: 'body'
